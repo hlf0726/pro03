68d217461569817625f71a3871a901841f479e55
helf <1071280664@qq.com>
2022-07-17 16:59:54 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println(111111111);
+        System.out.println(00000000);
     }
 }
