6888caf0363078f218c27ebb0b0fa08b786db71b
helf <1071280664@qq.com>
2022-07-17 17:08:18 +0800

    hotfix 1 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println(111111111);
         System.out.println(00000000);
+        System.out.println(3333333);
     }
 }
