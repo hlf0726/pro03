bca36badcf33a2bb1b50a0f105318629889df640
hlf0726 <109459887+hlf0726@users.noreply.github.com>
2022-07-17 17:59:52 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println(111111111);
         System.out.println(22222222);
         System.out.println(3333333);
+        System.out.println("push test");
     }
 }
